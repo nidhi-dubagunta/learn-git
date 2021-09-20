10c853c89bb6f44be089981a0bb95aae994ebd19
nidhi-dubagunta <70409151+nidhi-dubagunta@users.noreply.github.com>
2021-09-20 13:55:03 -0500

    Engine.Java modified

--- Engine.java
+++ Engine.java
@@ -52,14 +52,23 @@ public class Engine {
   // TODO: Complete this method
   // Player is either 0 or 1, row and column are valid and empty
   public void playerTurn(int player, int row, int col) {
-    return;
+	  
+	  board[row][col] = symbols[player];
+	  
+	 
   }
 
   // TODO: Complete this method
   // Return true if the board is full
   // Return false if not
   public boolean checkTie() {
-    return true;
+	  for (int row = 0; row < BOARD_SIZE; row++) {
+	      for (int col = 0; col < BOARD_SIZE; col++) {
+	        if(board[row][col].equals(" ")
+	        		return false;
+	      }
+	    }
+	  return true;
   }
 
   public int checkWinner() {
